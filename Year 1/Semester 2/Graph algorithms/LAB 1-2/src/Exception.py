9f7a1d688e02c03d25d5b3a6ced191bdfd1fae8a
915_Moldovan_Daniel_Gelu <73030496+dany002@users.noreply.github.com>
2022-05-04 22:32:26 +0300

    initial commit

---
+++ Year 1/Semester 2/Graph algorithms/LAB 1-2/src/Exception.py
@@ -0,0 +1,19 @@
+
+
+class Error(Exception):
+    pass
+
+class VertexError(Error):
+    pass
+
+class EdgeError(Error):
+    pass
+
+class FileError(Error):
+    pass
+
+class VertexOrEdge(Error):
+    pass
+
+class YesOrNo(Error):
+    pass
