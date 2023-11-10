457a98147c99ee780556364c42f5995ed6ab5208
root <root@SenZatIonALL.rdsnet.ro>
2023-11-10 14:04:00 +0200

    initial commit

--- Year 3/Semester 1/PDP/LAB1/SalesManager.cpp
+++
@@ -1,8 +0,0 @@
-// SalesManager.cpp
-#include "SalesManager.h"
-
-SalesManager::SalesManager(Inventory& inventory) : inventory(inventory) {}
-
-void SalesManager::performSale(const std::vector<std::pair<int, int>>& items) {
-    inventory.sellProducts(items);
-}
