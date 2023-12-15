3cb53c8510a17d2dbb57daf9afae1dc548b5f527
dany002 <moldovandangelu@gmail.com>
2023-12-15 16:53:53 +0200

    initial commit

---
+++ Year3/Semester1/PDP/LAB5/LAB5/src/main/java/org/example/Main.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import org.example.Algorithms.ParallelClassic;
+import org.example.Algorithms.ParallelKaratsuba;
+import org.example.Algorithms.SequentialClassic;
+import org.example.Algorithms.SequentialKaratsuba;
+import org.example.Model.AlgorithmChoice;
+import org.example.Model.MethodChoice;
+import org.example.Model.Polynomial;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+public class Main {
+    private static final MethodChoice METHOD = MethodChoice.PARALLEL;
+    private static final AlgorithmChoice ALGORITHM = AlgorithmChoice.KARATSUBA;
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
+        Polynomial p1 = new Polynomial(10);
+        System.out.println("p1=" + p1);
+        Polynomial p2 = new Polynomial(20);
+        System.out.println("p2=" + p2);
+        long startTime = System.nanoTime();
+        run(p1, p2);
+        long endTime = System.nanoTime();
+        double duration = ((double) endTime - (double) startTime) / 1_000_000_000.0;
+        BufferedWriter out = new BufferedWriter(
+                new FileWriter("results.txt", true));
+        out.write(String.format("Duration for %s - degree %s and %s: %s seconds", METHOD + " " + ALGORITHM, p1.getDegree(), p2.getDegree(),duration));
+        out.newLine();
+        out.close();
+        System.out.println("Duration: " + duration + " seconds");
+    }
+
+    private static void run(Polynomial p1, Polynomial p2) throws InterruptedException, ExecutionException {
+        Polynomial result;
+        switch (METHOD) {
+            case SEQUENTIAL:
+                if (ALGORITHM.equals(AlgorithmChoice.CLASSIC)) {
+                    result = SequentialClassic.multiply(p1, p2);
+                } else {
+                    result = SequentialKaratsuba.multiply(p1, p2);
+                }
+                break;
+            case PARALLEL:
+                if (ALGORITHM.equals(AlgorithmChoice.CLASSIC)) {
+                    result = ParallelClassic.multiply(p1, p2);
+                } else {
+                    result = ParallelKaratsuba.multiply(p1, p2, 1);
+                }
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + METHOD);
+        }
+        System.out.println("result=" + result);
+    }
+}
---
+++ Year3/Semester1/PDP/LAB5/LAB5/src/main/java/org/example/Model/AlgorithmChoice.java
@@ -0,0 +1,6 @@
+package org.example.Model;
+
+public enum AlgorithmChoice {
+    CLASSIC,
+    KARATSUBA
+}
